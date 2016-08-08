97027547c6be0cb39a03ec834374777659939d59
Sean Perez <sean.perez29@gmail.com>
2016-08-08 13:55:34 -0700

    Refactor image thumbnails and stringFromPlacemark method

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -42,7 +42,7 @@ class LocationCell: UITableViewCell {
     
     func imageForLocation(location: Location) -> UIImage {
         if location.hasPhoto, let image = location.photoImage {
-            return image
+            return image.resizedImageWithBound(CGSize(width: 52, height: 52))
         }
         return UIImage()
     }
